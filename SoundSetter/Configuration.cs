8bf7dd81d73216efe3e21aa3f7561751a944c1ef
BoxuChan <enzolefebvre@gmail.com>
2025-12-21 09:03:51 +0000

    Update for 7.4 & API 14

--- SoundSetter/Configuration.cs
+++ SoundSetter/Configuration.cs
@@ -10,7 +10,7 @@ namespace SoundSetter
         public int Version { get; set; }
 
         [Obsolete]
-        public VirtualKey.Enum[] Keybind { get; }
+        public VirtualKey.Enum[]? Keybind { get; }
 
         public VirtualKey.Enum ModifierKey { get; set; }
         public VirtualKey.Enum MajorKey { get; set; }
@@ -23,23 +23,23 @@ namespace SoundSetter
             MajorKey = VirtualKey.Enum.VkK;
         }
 
-        [JsonIgnore] private IDalamudPluginInterface pluginInterface;
+        [JsonIgnore] private IDalamudPluginInterface? pluginInterface;
 
+        [Obsolete("Obsolete")]
         public void Initialize(IDalamudPluginInterface pluginInterface)
         {
             this.pluginInterface = pluginInterface;
 
             // v1.0.2 compat
             if (ModifierKey != default) return;
-#pragma warning disable 612
+            if (Keybind == null) return;
             ModifierKey = Keybind[0];
             MajorKey = Keybind[1];
-#pragma warning restore 612
         }
 
         public void Save()
         {
-            this.pluginInterface.SavePluginConfig(this);
+            this.pluginInterface?.SavePluginConfig(this);
         }
     }
 }
